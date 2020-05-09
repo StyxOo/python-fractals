4b244e068c5824b91246ce28d8589d2b65c7328a
StyxOo <l.rothenhaeusler@gmail.com>
2020-05-09 16:39:08 +0200

    Added support for jump characters and tweaked test parameters

--- drawer.py
+++ drawer.py
@@ -1,5 +1,7 @@
 import turtle
 
+positions = []
+
 
 def draw_fractal(fractal, settings, distance):
     t = turtle.Turtle()  # create the turtle
@@ -8,7 +10,7 @@ def draw_fractal(fractal, settings, distance):
     t.up()
     t.back(200)
     t.down()
-    t.speed(9)
+    t.speed(0)
 
     for c in fractal:
         if c in settings['variables']:
@@ -17,6 +19,25 @@ def draw_fractal(fractal, settings, distance):
             t.right(settings['angle'])
         elif c == '-':
             t.left(settings['angle'])
+        elif c == '[':
+            save_position(t)
+        elif c == ']':
+            jump_to_last_position(t)
         else:
             print("Unknown character '{}' in fractal".format(c))
     wn.exitonclick()
+
+
+def save_position(t):
+    position = {'x': t.xcor(), 'y': t.ycor(), 'heading': t.heading()}
+    positions.append(position)
+
+
+def jump_to_last_position(t):
+    position = positions[-1]
+    positions.pop()
+    t.penup()
+    t.setx(position['x'])
+    t.sety(position['y'])
+    t.setheading(position['heading'])
+    t.pendown()
--- fractal.py
+++ fractal.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     for fractal in settings:
         string = processor.process_fractal(fractal)
         print("Fractal '{}' after {} iterations is: {}".format(fractal['name'], fractal['n'], string))
-        if fractal['name'] == 'Koch Curve':
-            drawer.draw_fractal(string, fractal, 5)
+        if fractal['name'] == 'Plant':
+            drawer.draw_fractal(string, fractal, 4)
